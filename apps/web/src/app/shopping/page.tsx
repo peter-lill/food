a932a7d0fab2b94deb1d992fad618e4b9498d5d5
peter-lill <71703538+peter-lill@users.noreply.github.com>
2026-07-23 23:00:15 +1000

    Pass product catalogue to Shopping workspace

--- apps/web/src/app/shopping/page.tsx
+++ apps/web/src/app/shopping/page.tsx
@@ -48,6 +48,7 @@ export default async function ShoppingPage({
       <ShoppingWorkspace
         lists={workspace.lists}
         pantrySuggestions={workspace.pantrySuggestions}
+        products={workspace.products}
         selectedList={selectedList}
       />
     </>
